c3f26eaf148974959139ddd0f35290c1ac91d10a
Connor Brewster <cbrewster@users.noreply.github.com>
2022-03-08 01:40:21 +0000

    Update examples

--- README.md
+++ README.md
@@ -3,11 +3,41 @@
 Node.js bindings to Lua.
 
 ```javascript
-const { Lua } = require('@cbrewster/luna');
+import { Lua, LuaTable } from '@cbrewster/luna';
 
 (async () => {
-    const lua = new Lua();
-    const result = await lua.doString("return 1 + 2");
-    console.log(result); // 3
+  const lua = new Lua();
+
+  console.log(await lua.doString("x = 1")); // null
+  console.log(await lua.doString("return x")); // 1
+
+  try {
+    await lua.doString("this is a syntax error"); // Error!
+  } catch (e) {
+    console.log(e); 
+  }
+  
+  console.log(await lua.doString("return 1 + 2")); // 3
+  console.log(await lua.doString(`return "abc"`)); // "abc"
+
+  const table = await lua.doString(`return {key = "value", foo = "bar"}`);
+  if (table instanceof LuaTable) {
+    table.forEach((key, val) => {
+      console.log(`key: ${key} val: ${val}`);
+    });
+  }
+  
+  const selfReference = await lua.doString("x = {} \n x.self = x \n return x");
+  if (selfReference instanceof LuaTable) {
+    selfReference.forEach((key, val) => {
+      console.log(`key: ${key} val: ${val}`);
+      if (val?.toString() === selfReference.toString()) {
+        console.log("we have a cycle!");
+        console.log("Table: ", selfReference.toString());
+      }
+    });
+  }
+
+  lua.close();
 })()
 ```
--- examples/index.js
+++ examples/index.js
@@ -1,20 +1,37 @@
 const { Lua, LuaTable } = require('../');
 
 (async () => {
-    const lua = new Lua();
-    const table = await lua.doString("x = {}; x.self = x; return x");
-    console.log(table instanceof LuaTable);
+  const lua = new Lua();
+
+  console.log(await lua.doString("x = 1")); // null
+  console.log(await lua.doString("return x")); // 1
+
+  try {
+    await lua.doString("this is a syntax error"); // Error!
+  } catch (e) {
+    console.log(e); 
+  }
+  
+  console.log(await lua.doString("return 1 + 2")); // 3
+  console.log(await lua.doString(`return "abc"`)); // "abc"
+
+  const table = await lua.doString(`return {key = "value", foo = "bar"}`);
+  if (table instanceof LuaTable) {
     table.forEach((key, val) => {
-        console.log(`key: ${key}, val: ${val}`);
-        console.log(val instanceof LuaTable);
-        console.log("table equalness", table === val);
-        val.forEach((key, val) => {
-            console.log(`key: ${key}, val: ${val}`);
-        });
+      console.log(`key: ${key} val: ${val}`);
     });
+  }
+  
+  const selfReference = await lua.doString("x = {} \n x.self = x \n return x");
+  if (selfReference instanceof LuaTable) {
+    selfReference.forEach((key, val) => {
+      console.log(`key: ${key} val: ${val}`);
+      if (val?.toString() === selfReference.toString()) {
+        console.log("we have a cycle!");
+        console.log("Table: ", selfReference.toString());
+      }
+    });
+  }
 
-    const nil = await lua.doString("nil");
-    console.log('nil:', nil);
-
-    lua.close();
-})();
+  lua.close();
+})()
